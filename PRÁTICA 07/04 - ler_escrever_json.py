237359b38623a6da24a0ef6ce04cb6e5a40b58d0
VICTOR95MOURA <vittor.moura@live.com>
2025-09-02 11:30:26 -0300

    PRÁTICA 07

---
+++ PRÁTICA 07/04 - ler_escrever_json.py
@@ -0,0 +1,35 @@
+"""
+Crie um script em Python que leia e escreva dados em um arquivo JSON.
+O arquivo JSON deve conter informações de uma pessoa, com campos nome, idade e cidade.
+"""
+
+import json
+
+def ler_json(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo_json:
+            dados = json.load(arquivo_json)
+            print(dados)
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")       
+
+def escrever_json(nome_arquivo, dados):
+    try:
+        with open(nome_arquivo, 'w', encoding='utf-8') as arquivo_json:
+            json.dump(dados, arquivo_json)
+        print(f"Dados salvos em {nome_arquivo}.")    
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo {e}.")
+
+dados = {
+    "nome": "Jõao",
+    "idade": 25,
+    "cidade": "Recife"
+}
+
+if __name__ == "__main__":
+    nome_arquivo = input("Digite o nome do arquivo JSON: ").strip()
+    if not nome_arquivo.endswith('.json'):
+        nome_arquivo += '.json'
+    escrever_json(nome_arquivo, dados)
+    ler_json(nome_arquivo)
